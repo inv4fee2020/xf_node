b361e7ef3035fd31333d4efa3c92a113e100e73f
inv4fee2020 <inv4fee2020@gmail.com>
2023-11-04 14:24:50 +0000

    docker

--- setup.sh
+++ setup.sh
@@ -141,7 +141,7 @@ FUNC_CLONE_NODE_SETUP(){
 replace_text="\
      networks:
       - mynetwork
-        ipv4_address: 172.19.0.2
+    ipv4_address: 172.19.0.2
     ports:
       - "30303:30303"
 networks:
