bdf79d3624946495bade0a744ba44154ede2ff9f
inv4fee2020 <inv4fee2020@gmail.com>
2023-11-06 17:55:06 +0000

    nginx

--- setup.sh
+++ setup.sh
@@ -321,10 +321,17 @@ FUNC_NODE_DEPLOY(){
 
     # Create a test index.html page
     test_html="/var/www/html/index.html"
-    sudo echo "<html><head><title>Welcome to $A_RECORD</title></head><body><h1>Welcome to $A_RECORD</h1></body></html>" > "$test_html"
+    if [ ! -e $test_html ]; then
+        sudo touch $test_html
+        sudo echo "<html><head><title>Welcome to $A_RECORD</title></head><body><h1>Welcome to $A_RECORD</h1></body></html>" > "$test_html"
+    else
+        sudo mv $test_html "$test_html.orig"
+        sudo touch $test_html
+        sudo echo "<html><head><title>Welcome to $A_RECORD</title></head><body><h1>Welcome to $A_RECORD</h1></body></html>" > "$test_html"
+    fi
 
     # Request and install a Let's Encrypt SSL/TLS certificate for Nginx
-    sudo certbot --nginx -d "$USER_DOMAINS" -m "inv4fee2020@gmail.com"
+    sudo certbot --nginx  -m "inv4fee2020@gmail.com" -n --agree-tos -d "$USER_DOMAINS"
 
     # Get the source IP of the current SSH session
     source_ip=$(echo $SSH_CONNECTION | awk '{print $1}')
